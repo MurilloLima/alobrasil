667df076bee44cdf445102fa212b5cb1a018bb94
Murillo Lima <murillo_smit@hotmail.com>
2025-05-29 12:19:53 -0300

    imagem principal view home view

--- resources/views/home/pages/imoveis/view.blade.php
+++ resources/views/home/pages/imoveis/view.blade.php
@@ -13,27 +13,29 @@
 </style>
 
 @section('content')
-    <div class="container-fluid py-5 my-5">
+    <div class="container-fluid my-5">
         <div class="container py-5">
             <div class="container gallery-container">
 
-                <h1>{{ $data->endereco }}</h1>
+                <h1>{{ $data->tipo->name }},{{ $data->endereco }}</h1>
 
-                <p class="page-description text-left">Clique na image para amplia-la</p>
+                <p class="page-description text-left">Clicar na imagem para vê-la ampliada</p>
 
 
             </div>
-
             <div class="row">
                 <div class="col-md-7">
-                    <div class="tz-gallery">
-                        <div class="row">
-                            @foreach ($data->images as $item)
-                                <a class="lightbox" href="{{ asset('upload/imoveis/' . $item->image) }}">
-                                    <img src="{{ asset('upload/imoveis/' . $item->image) }}" width="200" alt="Bridge">
-                                </a>
-                            @endforeach
+                    <div class="row">
+                        <div class="col-md-12">
+                            <img src="{{ asset('upload/imoveis/' . $data->image) }}" alt="">
                         </div>
+                    </div>
+                    <div class="tz-gallery">
+                        @foreach ($data->images as $item)
+                            <a class="lightbox" href="{{ asset('upload/imoveis/' . $item->image) }}">
+                                <img src="{{ asset('upload/imoveis/' . $item->image) }}" width="200" alt="Bridge">
+                            </a>
+                        @endforeach
                     </div>
 
                 </div>
@@ -41,11 +43,11 @@
                     <h5 class="text-primary">Imóvel {{ $data->tipo->name }}</h5>
                     {{-- <h1 class="mb-4">{{ $data->endereco }}</h1> --}}
                     <p>{{ $data->city }}</p>
-                    <p>Area m² {{ $data->area }}</p>
-                    <p>Quartos {{ $data->quartos }}</p>
-                    <p>Banheiros {{ $data->banheiros }}</p>
-                    <p>Garagem {{ $data->garagem }}</p>
-                    <h2>R${{ $data->valor }}</h2>
+                    <p>Area m²: {{ $data->area }}</p>
+                    <p>Quartos: {{ $data->quartos }}</p>
+                    <p>Banheiros: {{ $data->banheiros }}</p>
+                    <p>Garagem: {{ $data->garagem }}</p>
+                    <h2>R$:{{ $data->valor }}</h2>
                     <p class="mb-4">{{ $data->desc }}</p>
                     <a href="https://wa.me//559982854848?text=Tenho%20interesse%20em%20comprar%20seu%20imóvel"
                         class="btn btn-secondary rounded-pill px-5 py-3 text-white">Tenho interesse</a>
