58b85e0ece91c695cec8bf7ed5907e54d2c86919
Murillo Lima <murillo_smit@hotmail.com>
2025-08-01 09:00:36 -0300

    modal videos images

--- resources/views/home/layout/app.blade.php
+++ resources/views/home/layout/app.blade.php
@@ -10,7 +10,9 @@
     <!-- Favicon -->
     <link rel="icon" href="{{ asset('home/img/icon.png') }}" sizes="any">
     <link rel="icon" type="image/png" href="{{ asset('home/img/icon.png') }}" rel="icon">
-
+    {{-- modal --}}
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css"
+        integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <!-- Google Web Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -62,7 +64,113 @@
     <script>
         baguetteBox.run('.tz-gallery');
     </script>
+    {{-- modal --}}
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js"
+        integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js"
+        integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous">
+    </script>
 
+    {{-- script modal --}}
+    <script>
+        var player
+        var staticPlayer
+
+        function onYouTubeIframeAPIReady() {
+            console.log('onYouTubeIframeAPIReady')
+            player = new YT.Player('player', {
+                videoId: 'M7lc1UVf-VE',
+                playerVars: {
+                    'playsinline': 1
+                },
+                events: {
+                    'onReady': onPlayerReady,
+                    'onStateChange': onPlayerStateChange
+                }
+            })
+        }
+
+        function onPlayerReady(event) {
+            event.target.playVideo() // autostart
+        }
+
+        var done = false;
+
+        function onPlayerStateChange(event) {
+            if (event.data == YT.PlayerState.PLAYING && !done) {
+                // do other custom stuff here
+                //setTimeout(stopVideo, 6000);
+                //done = true;
+            }
+        }
+
+        function stopVideo() {
+            player.stopVideo()
+        }
+
+        function loadYouTubeVideo() {
+            // 2. This code loads the IFrame Player API code asynchronously.
+            var tag = document.createElement('script');
+            tag.src = "https://www.youtube.com/iframe_api";
+            var firstScriptTag = document.getElementsByTagName('script')[0];
+            firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+        }
+
+        var dynamicVideoModal = document.getElementById('dynamicVideoModal')
+        dynamicVideoModal.addEventListener('show.bs.modal', function(event) {
+            // dynamically create video inside modal
+            // loadYouTubeVideo()
+        })
+        dynamicVideoModal.addEventListener('hidden.bs.modal', event => {
+            player.stopVideo()
+        })
+
+
+        // manual controls outside YT
+        const playBtn = document.getElementById('playBtn')
+        playBtn.addEventListener('click', function(event) {
+            player.playVideo()
+        })
+
+        const pauseBtn = document.getElementById('pauseBtn')
+        pauseBtn.addEventListener('click', function(event) {
+            console.log('pause')
+            player.pauseVideo()
+        })
+
+        const myModalEl = document.getElementById('videoModal')
+        myModalEl.addEventListener('show.bs.modal', event => {
+            staticPlayer = new YT.Player('staticPlayer')
+        })
+        myModalEl.addEventListener('hidden.bs.modal', event => {
+            staticPlayer.stopVideo()
+        })
+    </script>
+    <script>
+        $(document).ready(function() {
+            // Quando um link ou botão com a classe 'open-video-modal' for clicado
+            $('.open-video-modal').click(function(e) {
+                e.preventDefault(); // Previne o comportamento padrão do link
+                var videoUrl = $(this).data('video-url'); // Obtém a URL do vídeo do atributo data-video-url
+                var videoEmbedCode = '<iframe width="560" height="315" src="' + videoUrl +
+                    '" frameborder="0" allowfullscreen></iframe>';
+
+                $('#videoModal .modal-body').html(
+                videoEmbedCode); // Insere o código de incorporação no modal
+                $('#videoModal').modal('show'); // Exibe o modal
+            });
+
+            // Opcional: Limpar o conteúdo do modal ao fechar
+            $('#videoModal').on('hidden.bs.modal', function(e) {
+                $('#videoModal .modal-body').html('');
+            });
+        });
+    </script>
 </body>
 
 </html>
--- resources/views/home/pages/imoveis/view.blade.php
+++ resources/views/home/pages/imoveis/view.blade.php
@@ -43,32 +43,79 @@
                     <a href="https://wa.me//559982854848?text=Tenho%20interesse%20em%20comprar%20seu%20imóvel"
                         class="btn btn-secondary rounded-pill px-5 py-3 text-white">Tenho interesse</a>
                 </div>
-                <div class="row">
-                    <div class="col-md-12">
-                        {{-- <p>Veja os vídeos</p> --}}
-                        @foreach ($data->images as $item)
-                            <a class="lightbox" href="{{ asset('upload/imoveis/' . $item->image) }}">
-                                <div class="image">
-                                    <img src="{{ asset('upload/imoveis/' . $item->image) }}" alt=""
-                                        style="width: 200px; height: 140px;">
-                                </div>
-                            </a>
-                        @endforeach
-                    </div>
-                </div>
+                {{-- <p>Veja os vídeos</p> --}}
 
-            </div>
-            <div class="col-md-12">
-                @foreach ($data->videos as $item)
-                    <a class="lightbox" href="{{ asset('upload/video/' . $item->file) }}">
-                        <div class="video">
-                            <video src="{{ asset('upload/video/' . $item->file) }}"
-                                style="width: 200px; height: 140px;"></video>
+
+                <div class="row g-5 justify-content-center">
+                    @foreach ($data->images as $item)
+                        <div class="col-lg-6 col-xl-4 wow fadeIn" data-wow-delay=".3s"
+                            style="visibility: visible; animation-delay: 0.3s; animation-name: fadeIn;">
+                            <div class="blog-item position-relative bg-light rounded" style="margin-top: 50px;">
+                                <a href="" data-toggle="modal"
+                                    data-target=".bd-example-modal-lg{{ $item->id }}">
+                                    <img src="{{ asset('upload/imoveis/' . $item->image) }}"
+                                        class="img-fluid w-100 rounded-top" alt=""
+                                        style="width: 100px; height: 140px;">
+                                </a>
+                            </div>
                         </div>
-                    </a>
-                @endforeach
-            </div>
+                        {{-- modal --}}
+                        <div class="modal fade bd-example-modal-lg{{ $item->id }}" tabindex="-1" role="dialog"
+                            aria-labelledby="myLargeModalLabel" aria-hidden="true">
+                            <div class="modal-dialog modal-lg">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title" id="videoModalLabel">Título do Vídeo</h5>
+                                        <button type="button" class="close" data-dismiss="modal" aria-label="Fechar">
+                                            <span aria-hidden="true">&times;</span>
+                                        </button>
+                                    </div>
+                                    <div style="widows: 600px; height: 450px;">
+                                        <img src="{{ asset('upload/imoveis/' . $item->image) }}" data-toggle="modal"
+                                            data-target=".bd-example-modal-lg{{ $item->id }}"
+                                            class="img-fluid w-100 rounded-top" alt="">
+                                    </div>
 
+                                </div>
+                            </div>
+                        </div>
+                    @endforeach
+
+                    @foreach ($data->videos as $item)
+                        <div class="col-lg-6 col-xl-4 wow fadeIn" data-wow-delay=".3s"
+                            style="visibility: visible; animation-delay: 0.3s; animation-name: fadeIn;">
+                            <div class="blog-item position-relative bg-light rounded" style="margin-top: 50px;">
+                                <a href="" data-toggle="modal"
+                                    data-target=".bd-example-modal-lg{{ $item->id }}">
+                                    <img src="{{ asset('upload/video/' . $item->file) }}"
+                                        class="img-fluid w-100 rounded-top" alt=""
+                                        style="width: 100px; height: 140px;">
+                                </a>
+                            </div>
+                        </div>
+
+                        {{-- modal --}}
+                        <div class="modal fade bd-example-modal-lg{{ $item->id }}" tabindex="-1" role="dialog"
+                            aria-labelledby="myLargeModalLabel" aria-hidden="true">
+                            <div class="modal-dialog modal-lg">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title" id="videoModalLabel">{{ $item->title }}</h5>
+                                        <button type="button" class="close" data-dismiss="modal" aria-label="Fechar">
+                                            <span aria-hidden="true">&times;</span>
+                                        </button>
+                                    </div>
+                                    <div class="text-center">
+                                        <iframe src="{{ asset('upload/video/' . $item->file) }}" width="600" height="450" frameborder="0"></iframe>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    @endforeach
+
+
+                </div>
+            </div>
         </div>
     </div>
     <div class="container py-5">
