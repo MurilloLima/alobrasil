0135a2504d82f122d19744aa9946ec4b87d5c663
Murillo Lima <murillo_smit@hotmail.com>
2025-08-01 09:21:27 -0300

    ok alo brasil

--- resources/views/home/pages/imoveis/view.blade.php
+++ resources/views/home/pages/imoveis/view.blade.php
@@ -65,7 +65,7 @@
                             <div class="modal-dialog modal-lg">
                                 <div class="modal-content">
                                     <div class="modal-header">
-                                        <h5 class="modal-title" id="videoModalLabel">Título do Vídeo</h5>
+                                        <h5 class="modal-title" id="videoModalLabel">Alô Brasil</h5>
                                         <button type="button" class="close" data-dismiss="modal" aria-label="Fechar">
                                             <span aria-hidden="true">&times;</span>
                                         </button>
